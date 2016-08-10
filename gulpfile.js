018740f7eb9aeb7e4b965ae254dfc558096182fa
Joe Crick <joe@bitovi.com>
2016-08-10 11:55:21 -0700

    (bugfix/j-crick-fe-challenge): Refactor docs task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 var requireDir = require('require-dir');
 
-requireDir('./build_tasks', { recurse: true });
+requireDir('./build_tasks', {recurse: true});
 
 gulp.task('dev', ['styles:dev', 'scripts:dev', 'copy:dev', 'browser-sync']);
+gulp.task('build', ['test', 'docs', 'styles:dev', 'scripts:dev', 'copy']);
